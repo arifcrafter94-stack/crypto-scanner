bd5628e97704e73bddaecb34ac55f85a9d333574
arifcrafter94-stack <arifcrafter94@gmail.com>
2026-06-02 13:37:20 +0800

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,25 @@
-print("Bot berjalan!")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = """
+🚀 Crypto Scanner Aktif
+
+GitHub Actions berjaya berjalan.
+
+Ini adalah mesej ujian.
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Telegram message sent")
